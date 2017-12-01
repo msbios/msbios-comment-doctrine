59cf9c1fd65578a2d334b195abbf08817c459cc7
Judzhin Miles <info@msbios.com>
2017-12-01 19:31:37 +0200

    Lazy Composer Commiter

---
+++ src/Module.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Comment\Doctrine;
+
+use MSBios\ModuleInterface;
+use Zend\EventManager\EventInterface;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+
+/**
+ * Class Module
+ * @package MSBios\Application
+ */
+class Module implements
+    ModuleInterface,
+    AutoloaderProviderInterface,
+    BootstrapListenerInterface
+{
+    /** @const VERSION */
+    const VERSION = '1.0.5';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            AutoloaderFactory::STANDARD_AUTOLOADER => [
+                StandardAutoloader::LOAD_NS => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+
+
+    /**
+     * Listen to the bootstrap event
+     *
+     * @param EventInterface $e
+     * @return array
+     */
+    public function onBootstrap(EventInterface $e)
+    {
+        // TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // Hard override
+        $e->getTarget()
+            ->getServiceManager()
+            ->get('ViewHelperManager')
+            ->get('partial')
+            ->setView($e->getTarget()
+                ->getServiceManager()->get('Zend\View\Renderer\PhpRenderer'));
+    }
+}
