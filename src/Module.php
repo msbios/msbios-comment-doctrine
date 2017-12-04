f320f376973c2a6b9134394b8a331d6938b9a2b2
Judzhin Miles <info@msbios.com>
2017-12-04 21:16:24 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -22,7 +22,7 @@ class Module implements
     BootstrapListenerInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     public function getConfig()
     {
